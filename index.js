178cc1ef57bcc2dc3e85a2620ce8cb8089bf2168
michael furst <michael@furst.us>
2018-05-15 23:48:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var specialGuest = "Neil deGrasse Tyson!!"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about Let's talk about space.";
+var topic = "space";
 
-var conversation = `${topic}`;
-
-
-'${greetSpecialGuest}! ${topic}'
+var conversation = `Let/'s talk about ${topic}`;
